e9522a64fcc7cc9069e7d06aec033f899b3db044
robernom <rober95@hotmail.com>
2016-10-17 10:42:01 +0200

    Ejercicio 6

--- client.py
+++ client.py
@@ -10,23 +10,18 @@ import sys
 # Constantes. Dirección IP del servidor y contenido a enviar
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
-LINE = sys.argv[3:len(sys.argv)]
+LINE = sys.argv[3:]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    try:
-        for word in LINE:
-            print("Enviando:", word)
-    except:
-        print("No hay texto")
-    entire = ""
-    for word in LINE:
-        entire += word + ' '
-
-    my_socket.send(bytes(entire, 'utf-8') + b'\r\n')
-
-    data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
-
+    entire = ' '.join(LINE)
+    if entire != "":
+        if LINE[0] == "register":
+            entire = "REGISTER sip:" + LINE[1] + " SIP/2.0\r\n\r\n"
+        my_socket.send(bytes(entire, 'utf-8') + b'\r\n')
+        data = my_socket.recv(1024)
+        print(data.decode('utf-8'))
+    else:
+        print("No hay mensaje =>")
 print("Socket terminado.")
