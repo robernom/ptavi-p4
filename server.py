85d80dc46d06b4476a6455eee7b71b95d88ca4e7
robernom <rober95@hotmail.com>
2016-10-24 09:35:37 +0200

    PEP8 y PEP257

--- server.py
+++ server.py
@@ -10,14 +10,15 @@ from time import time, gmtime, strftime
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """Clase para un servidor SIP."""
 
-    def json2registered():
+    data_dicc = {}
+
+    def json2registered(self):
+        """Busca fichero JSON con clientes; si no hay devuelve dicc vacio."""
         try:
             with open('registered.json') as f_json:
-                return json.load(f_json)
-        except:
-            return {}
-
-    data_dicc = json2registered()
+                self.data_dicc = json.load(f_json)
+        except FileNotFoundError:
+            self.data_dicc = {}
 
     def register2json(self):
         """Introduce en un fichero JSON los usuarios."""
@@ -26,39 +27,31 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
     def handle(self):
         """Cada vez que un cliente envia una peticion se ejecuta."""
-
         data = self.rfile.read().decode('utf-8')
         print(data)
-        client_data = data.split()
-        user = client_data[1][4:]
-        exval = int(client_data[4])
+        c_data = data.split()
+        user, exval, c_ip = c_data[1][4:], c_data[4], self.client_address[0]
         now = int(time())
         str_now = strftime('%Y-%m-%d %H:%M:%S', gmtime(now))
         time_exval = int(exval) + now
         str_exval = strftime('%Y-%m-%d %H:%M:%S', gmtime(time_exval))
-        self.data_dicc[user] = {'address': self.client_address[0], 
-                                'expires': str_exval}
-
+        self.data_dicc[user] = {'address': c_ip, 'expires': str_exval}
         self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-        
-
         lista_expirados = []
         for user in self.data_dicc:
             if self.data_dicc[user]['expires'] <= str_now:
                 lista_expirados.append(user)
         for name in lista_expirados:
             del self.data_dicc[name]
-
         self.register2json()
-        print(self.data_dicc)
 
 if __name__ == "__main__":
     try:
         PORT = int(sys.argv[1])
     except (IndexError, ValueError):
         sys.exit("Usage: server.py port_number")
-    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler)
+    SERV = socketserver.UDPServer(('', PORT), SIPRegisterHandler)
     try:
-        serv.serve_forever()
+        SERV.serve_forever()
     except KeyboardInterrupt:
         print("Finalizado servidor")
